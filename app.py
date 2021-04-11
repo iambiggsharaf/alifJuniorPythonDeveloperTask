fea591e555a9bad76fb1442ebdea2bfef4e46eec
iambiggsharaf <sharaf.nazarov24@gmail.com>
2021-04-11 10:12:22 +0500

    Commit #4: deleted unnecessary line split

--- app.py
+++ app.py
@@ -9,10 +9,8 @@ class AlifTask:
         r_positive = open("positive_results.txt", "w")
 
         for x in f:
-            a, b = x.split()
-
             try:
-                result = eval(a + self.operationType + b)
+                result = eval(x.replace(" ", self.operationType))
             except ZeroDivisionError:
                 continue
 
@@ -20,7 +18,7 @@ class AlifTask:
                 r_positive.write(str(result) + "\n")
             elif result < 0:
                 r_negative.write(str(result) + "\n")
-                
+
         f.close()
         r_negative.close()
         r_positive.close()
